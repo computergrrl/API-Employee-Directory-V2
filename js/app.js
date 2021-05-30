4b753276b763fc6db155fbfadb15a2adba387d69
computergrrl <anani.mowse@gmail.com>
2021-05-29 19:58:44 -0700

    created global vars

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&nat=US`;
+const modalContainer = document.querySelector(".modal-container");
+const gallery = document.querySelector("#gallery");
+//const modalContainer = document.querySelector(".modal-content");
+const modal = document.querySelector(".modal");
+const modalClose = document.querySelector(".modal-close");
